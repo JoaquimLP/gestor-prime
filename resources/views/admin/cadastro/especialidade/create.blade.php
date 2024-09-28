2c3696c101a8eed01c32e275bbe9f632f3b35b40
Joaquim Lopes <joaquim2013@gmail.com>
2024-09-28 09:27:55 -0300

    FIx

--- resources/views/admin/cadastro/especialidade/create.blade.php
+++ resources/views/admin/cadastro/especialidade/create.blade.php
@@ -5,6 +5,7 @@
     <div class="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
         <div class="flex flex-col gap-9">
             <x-form.form-layout :route="route('cadastro.especialidade.store')" :method="'POST'" :btnCancelRoute="route('cadastro.especialidade.index')">
+                @csrf
                 <x-form.form-col class="gap-1">
                     <x-form.input-text class="my-2 w-full" name="nome" placeholder="Teste" label="Nome"
                         value="" />
